29300996562c340e533fa8e33e7f28c12a5c08f4
NeoMike84 <74468017+NeoMike84@users.noreply.github.com>
2020-12-03 10:05:15 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,60 @@
+function robotTourneGauche () {
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, 25, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, 50, 67)
+}
+function robotTourneSpin () {
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 100, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, 50, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, 50, 67)
+}
+function robotTourneGaucheSurPlace () {
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 100, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, 50, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, 50, 67)
+}
+function robotRecule () {
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 100, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, 25, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, 100, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, 25, 67)
+}
+function robotAvance () {
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, 50, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, 50, 67)
+}
+function robotTourneDroite () {
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, 50, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, 25, 67)
+}
+function robotStop () {
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, 0, 67)
+}
+function robotTourneDroiteSurPlace () {
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED1, 0, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED2, 50, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED3, 100, 67)
+    PCA9685.setLedDutyCycle(PCA9685.LEDNum.LED4, 50, 67)
+}
+PCA9685.init(67, 0)
+let anneauLED = neopixel.create(DigitalPin.P5, 18, NeoPixelMode.RGB)
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    anneauLED.showRainbow(1, 360)
+})
